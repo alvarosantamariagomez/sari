c4d6bbd1e7841b1204207d33bdbe9cd118d529ce
asantamaria <alvaro.santamaria@get.omp.eu>
2024-01-02 13:45:34 +0100

    enabling optional packages

--- app.R
+++ app.R
@@ -51,18 +51,18 @@ check_load <- function(packages) {
 }
 
 # Shinyapps & local version
-suppressPackageStartupMessages(suppressMessages(suppressWarnings({
-  library(mvcwt, verbose = F, quietly = T) #v1.3.1
-  library(leaflet, verbose = F, quietly = T) #v2.1.2
-  library(geojsonio, verbose = F, quietly = T) #v0.11.3
-})))
+# suppressPackageStartupMessages(suppressMessages(suppressWarnings({
+#   library(mvcwt, verbose = F, quietly = T) #v1.3.1
+#   library(leaflet, verbose = F, quietly = T) #v2.1.2
+#   library(geojsonio, verbose = F, quietly = T) #v0.11.3
+# })))
 # GitHub version
-# optionalPackages <- c(
-#   "mvcwt",
-#   "leaflet",
-#   "geojsonio"
-# )
-# check_load(optionalPackages)
+optionalPackages <- c(
+  "mvcwt",
+  "leaflet",
+  "geojsonio"
+)
+check_load(optionalPackages)
 
 # GUI addons ####
 
