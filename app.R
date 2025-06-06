784114fd225db11e77c10c9bec60a84b42d4931a
asantamaria <alvaro.santamaria@get.omp.eu>
2025-06-06 12:33:17 +0200

    disabling app autoreload

--- app.R
+++ app.R
@@ -447,7 +447,7 @@ if (all(c("Rcpp", "RcppArmadillo", "RcppEigen") %in% .packages()) && Sys.info()[
 options(shiny.fullstacktrace = T, shiny.maxRequestSize = 60*1024^2, width = 280, max.print = 50)
 options(shiny.trace = F)
 devmode(F)
-options(shiny.autoreload = T, shiny.autoreload.pattern = "app.R")
+# options(shiny.autoreload = T, shiny.autoreload.pattern = "app.R")
 options(scipen = 4)
 Sys.setlocale('LC_ALL','C')
 options(shiny.reactlog = F)
